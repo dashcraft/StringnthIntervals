b13465cb678c34bbf7da0ee12a992777079e3e92
Daniel Ashcraft <ashcraftbookkeeping@gmail.com>
2015-10-10 19:20:40 -0500

    HackerRankStringManip
    String Manipulations from hacker rank...

---
+++ StringManip.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class StringManip {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String A = sc.next();
+		String B = sc.next();
+		int length = A.length() + B.length();
+		System.out.println(length);
+
+		if (A.compareTo(B) > 0) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+		}
+
+		System.out.println(A.substring(0, 1).toUpperCase() + A.substring(1)
+				+ " " + B.substring(0, 1).toUpperCase() + B.substring(1));
+
+	}
+}
